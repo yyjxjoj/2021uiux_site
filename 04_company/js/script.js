bab3014a0f1d5b15076d6005872dd40f163018d2
yyjxjoj <insane0114@gmail.com>
2021-05-17 14:42:03 +0900

    0517

--- 04_company/js/script.js
+++ 04_company/js/script.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
-  $(".slider").bxSlider({
-    pager:false,
+  let mainSlider = $(".main_slider").bxSlider({
+    controls: false,
+    pager: false,
+  });
+
+  $(".img_controls .controls .next_btn").click(function(){
+      mainSlider.goToNextSlide();
+  });
+  $(".img_controls .controls .prev_btn").click(function(){
+      mainSlider.goToPrevSlide();
+  });
+
+  $(".content_slider").bxSlider({
+    pager: true,
+    controls: false,
   });
 });
